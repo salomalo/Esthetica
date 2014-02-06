2d3e6209834b61a64132e77e0bfdcfc10d22e026
Francis Morissette <morissette.francis@gmail.com>
2014-02-05 23:42:40 -0500

    BUGFIX: Make credit class issue 10 Finished the Credit class

--- behind/classes/Credit.php
+++ behind/classes/Credit.php
@@ -1,10 +1,57 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Document sans titre</title>
-</head>
+<?php
 
-<body>
-</body>
-</html>
+class Credit {
+	private $_db,
+			$_data;
+	
+	public function __construct($credit = null) {
+		$this->_db = DB::getInstance();
+		
+		if($credit) {
+			$this->find($credit);
+		}
+	}
+	
+	public function create($fields = array()) {
+		$creditId = $this->_db->insert('credits', $fields);
+		if(!$creditId) {
+			throw new Exception('Un problème est survenu lors de la création du crédit.');
+		}
+		
+		return $creditId;
+	}
+	
+	public function find($credit = null) {
+		if($credit) {
+			$field = 'id';
+			$data = $this->_db->get('credits', array($field, '=', $credit));
+			
+			if($data->count()) {
+				$this->_data = $data->first();
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public function findAll($userId = null) {
+		if($userId) {
+			$field = 'user_id';
+			$data = $this->_db->get('credits', array($field, '=', $userId), '*', 'ORDER BY date DESC');
+			
+			if($data->count()) {
+				$this->_data = $data;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public function exists() {
+		return !empty($this->_data);
+	}
+	
+	public function data() {
+		return $this->_data;
+	}
+}
